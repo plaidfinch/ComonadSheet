8218b6043b47fe3e587946dbe4738bd0cdd07a69
Kenneth Foner <kenny.foner@gmail.com>
2013-11-25 22:44:34 -0500

    New numeric instances; sadly, requires UndecidableInstances and OverlappingInstances, but seems pretty safe in this case.

--- NumericInstances.hs
+++ NumericInstances.hs
@@ -1,14 +1,37 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances , FlexibleContexts , UndecidableInstances , OverlappingInstances #-}
 
 module NumericInstances where
 
 import Control.Applicative
 
 instance (Applicative f, Num a) => Num (f a) where
-   a + b  = (+) <$> a <*> b
-   a * b  = (*) <$> a <*> b
-   a - b  = (-) <$> a <*> b
+   (+)  = liftA2 (+)
+   (*)  = liftA2 (*)
+   (-)  = liftA2 (-)
    abs    = fmap abs
    signum = fmap signum
    fromInteger = pure . fromInteger
+
+instance (Enum a, Applicative f) => Enum (f a) where
+   succ           = fmap succ
+   pred           = fmap pred
+   toEnum         = pure . toEnum
+   fromEnum       = error "fromEnum: can't go from arbitrary functor to integer"
+
+instance (Ord (f a), Applicative f, Real a) => Real (f a) where
+   toRational     = error "toRational: can't go from arbitrary functor to rational"
+
+instance (Fractional a, Applicative f) => Fractional (f a) where
+   recip        = fmap recip
+   fromRational = pure . fromRational
+
+instance (Enum (f a), Real (f a), Applicative f, Integral a) => Integral (f a) where
+   quot          = liftA2 quot
+   rem           = liftA2 rem
+   div           = liftA2 div
+   mod           = liftA2 mod
+   x `quotRem` y = (x `quot` y, x `rem` y)
+   x `divMod`  y = (x `div`  y, x `mod` y)
+   toInteger     = error "toInteger: can't go from arbitrary functor to integer"
+
 
