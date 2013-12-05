81ffb699b1e2ba7fc74aaa04587aea247c5f5d9c
Kenneth Foner <kenny.foner@gmail.com>
2013-12-05 15:45:55 -0500

    Fixed redundancies in Ref type, and added a DynamicCell constructor for CellExprs, which is contagious (that is, if it's involved in an expression, the expression throws away reference information to become dynamic).

--- src/Checked.hs
+++ src/Checked.hs
@@ -19,64 +19,69 @@ import Data.Monoid
 import Data.Set (Set)
 import qualified Data.Set as Set
 
--- Abs case is actually un-needed and redundant... #TODO
-data Ref x = Abs x | Rel Int (Maybe x) deriving (Show, Eq, Ord)
+data Ref x = Abs Int x | Rel Int deriving (Show, Eq, Ord)
 
 instance Monoid (Ref x) where
-   mempty                    = Rel 0 Nothing
-   Abs x   `mappend` Rel y z = Rel y       (z <|> Just x)
-   Rel w x `mappend` Rel y z = Rel (w + y) (z <|> x)
-   _ `mappend` Abs x         = Abs x
+   mempty                    = Rel 0
+   Abs x y `mappend` Rel z   = Abs (x + z) y
+   Rel x   `mappend` Abs y z = Abs (x + y) z
+   Rel x   `mappend` Rel y   = Rel (x + y)
+   Abs y z `mappend` Abs _ _ = Abs y z
 
 at :: (c,r) -> (Ref c,Ref r)
-at = (Abs *** Abs)
+at = (Abs 0 *** Abs 0)
 
 atRow :: r -> (Ref c,Ref r)
-atRow = (mempty,) . Abs
+atRow = (mempty,) . Abs 0
 
 atCol :: c -> (Ref c,Ref r)
-atCol = (,mempty) . Abs
+atCol = (,mempty) . Abs 0
 
 aboveBy :: Int -> (Ref c,Ref r)
-aboveBy = (mempty,) . flip Rel Nothing . negate
+aboveBy = (mempty,) . Rel . negate
 
 above :: (Ref c,Ref r)
 above = aboveBy 1
 
 belowBy :: Int -> (Ref c,Ref r)
-belowBy = (mempty,) . flip Rel Nothing
+belowBy = (mempty,) . Rel
 
 below :: (Ref c,Ref r)
 below = belowBy 1
 
 leftBy :: Int -> (Ref c,Ref r)
-leftBy = (,mempty) . flip Rel Nothing . negate
+leftBy = (,mempty) . Rel . negate
 
 left :: (Ref c,Ref r)
 left = leftBy 1
 
 rightBy :: Int -> (Ref c,Ref r)
-rightBy = (,mempty) . flip Rel Nothing
+rightBy = (,mempty) . Rel
 
 right :: (Ref c,Ref r)
 right = rightBy 1
 
 type CellRef c r = (Ref c,Ref r)
 
-data CellExpr c r a b = CellExpr { getRefs :: Set (CellRef c r)
-                                 , appCell  :: Z2 c r a -> b }
+data CellExpr c r a b =
+   StaticCell  { getRefs :: Set (CellRef c r)
+               , appSCell :: Z2 c r a -> b } |
+   DynamicCell { appDCell :: Z2 c r a -> b }
 
 instance Functor (CellExpr c r a) where
-   fmap f (CellExpr refs a) = CellExpr refs (f . a)
+   fmap f (StaticCell refs a) = StaticCell refs (f . a)
+   fmap f (DynamicCell a)     = DynamicCell     (f . a)
 
 instance (Ord c, Ord r) => Applicative (CellExpr c r a) where
-   pure                          = CellExpr Set.empty . const
-   CellExpr r a <*> CellExpr s b = CellExpr (r <> s) (a <*> b)
+   pure                              = StaticCell Set.empty . const
+   StaticCell r a <*> StaticCell s b = StaticCell (r <> s) (a <*> b)
+   StaticCell _ a <*> DynamicCell  b = DynamicCell         (a <*> b)
+   DynamicCell  a <*> StaticCell _ b = DynamicCell         (a <*> b)
+   DynamicCell  a <*> DynamicCell  b = DynamicCell         (a <*> b)
 
 genericDeref :: (x -> a -> a) -> (Int -> a -> a) -> Ref x -> a -> a
-genericDeref absolute _        (Abs x)          = absolute x
-genericDeref absolute relative (Rel i (Just x)) = relative i . absolute x
-genericDeref _        relative (Rel i Nothing)  = relative i
+genericDeref _        relative (Rel i)   = relative i
+genericDeref absolute relative (Abs i x) = relative i . absolute x
 
 derefRow :: (Ord r, Enum r) => Ref r -> Z2 c r a -> Z2 c r a
 derefRow = genericDeref U.atRow U.belowBy
@@ -91,9 +96,9 @@ indexDeref = genericDeref const relative
       relative n z | otherwise = iterate pred z !! (negate n)
 
 cell :: (Ord c, Ord r, Enum c, Enum r) => CellRef c r -> CellExpr c r a a
-cell ref@(c,r) = CellExpr (Set.singleton ref) $ U.cell $ derefCol c . derefRow r
+cell ref@(c,r) = StaticCell (Set.singleton ref) $ U.cell $ derefCol c . derefRow r
 
 cells :: (Ord c, Ord r, Enum c, Enum r) => [CellRef c r] -> CellExpr c r a [a]
-cells refs = CellExpr (Set.fromList refs) $ sequence $ map (appCell . cell) refs
+cells refs = StaticCell (Set.fromList refs) $ sequence $ map (appSCell . cell) refs
 
 
