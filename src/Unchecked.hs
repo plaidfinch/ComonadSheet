e083e98d8d531750105d22674c22262e5c68d27f
Kenneth Foner <kenny.foner@gmail.com>
2013-12-20 13:58:58 -0500

    Use ComonadApply for evaluate rather than Applicative. It's equivalent, but it makes more sense this way.

--- src/Unchecked.hs
+++ src/Unchecked.hs
@@ -9,8 +9,8 @@ import Z4
 import Control.Arrow (first,second)
 import Data.Function
 
-evaluate :: (Applicative f, Comonad f) => f (f b -> b) -> f b
-evaluate fs = fix $ (fs <*>) . duplicate
+evaluate :: ComonadApply f => f (f b -> b) -> f b
+evaluate fs = fix $ (fs <@>) . duplicate
 --evaluate = extend wfix -- more elegant, but breaks sharing, resulting in exponential performance penalty
 
 cell :: (RefOf r z l, AnyZipper z i a) => r -> z -> a
