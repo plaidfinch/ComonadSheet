2e845a9f51c6a7b13d10fd5cf88604946a42dfcb
Kenneth Foner <kenny.foner@gmail.com>
2014-04-16 01:48:51 -0400

    Added some preliminary code showing how this library design eliminates the n^2 instance declaration problem, as well as making more elegant (and thoroughly commenting) the magical Comonad instance for the Compose of two Distributive Comonads -- it turns out only the inner one actually has to be Distributive.

--- src/ComonadComposition.hs
+++ src/ComonadComposition.hs
@@ -1,4 +1,4 @@
---{-# LANGUAGE UndecidableInstances, FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module ComonadComposition where
 
@@ -6,15 +6,57 @@ import Control.Comonad
 import Control.Arrow
 import Control.Applicative
 import Data.Distributive
+import Data.Functor
 import Data.Functor.Compose
 import Data.Stream
-import Prelude hiding ( map , tail , iterate , take , head )
 
-data Z a = Z (Stream a) a (Stream a)
+import Prelude hiding ( iterate , tail )
 
-zipL, zipR :: Z a -> Z a
-zipR (Z ls c (Cons r rs)) = Z (Cons c ls) r rs
-zipL (Z (Cons l ls) c rs) = Z ls l (Cons c rs)
+composedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
+composedly f = Compose . f . getCompose
+
+data Z a = Z (Stream a) a (Stream a) deriving ( Functor )
+
+class Zipper1 z where
+  zipL :: z a -> z a
+  zipR :: z a -> z a
+
+instance Zipper1 Z where
+  zipR (Z ls c (Cons r rs)) = Z (Cons c ls) r rs
+  zipL (Z (Cons l ls) c rs) = Z ls l (Cons c rs)
+
+instance (Functor f, Zipper1 z) => Zipper1 (Compose f z) where
+  zipL = composedly (fmap zipL)
+  zipR = composedly (fmap zipR)
+
+class Zipper2 z where
+  zipU :: z a -> z a
+  zipD :: z a -> z a
+
+instance (Zipper1 z) => Zipper2 (Compose z x) where
+  zipU = composedly zipL
+  zipD = composedly zipR
+
+class Zipper3 z where
+  zipI :: z a -> z a
+  zipO :: z a -> z a
+
+instance (Zipper2 z) => Zipper3 (Compose z x) where
+  zipI = composedly zipU
+  zipO = composedly zipD
+
+-- Some example zippers for testing...
+ints :: Z Int
+ints = zipIterate pred succ 0
+
+intPairs :: Compose Z Z (Int,Int)
+intPairs = (,) <$> Compose (     pure ints)
+               <*> Compose (fmap pure ints)
+
+intTriples :: Compose (Compose Z Z) Z (Int,Int,Int)
+intTriples = (,,) <$> (Compose . Compose) ((pure      .      pure) ints)
+                  <*> (Compose . Compose) ((pure      . fmap pure) ints)
+                  <*> (Compose . Compose) ((fmap pure . fmap pure) ints)
 
 viewL, viewR :: Z a -> Stream a
 viewL (Z ls _ _) = ls
@@ -31,9 +73,6 @@ zipUnfold :: (c -> (a,c)) -> (c -> a) -> (c -> (a,c)) -> c -> Z a
 zipUnfold prev center next =
    Z <$> unfold prev <*> center <*> unfold next
 
-instance Functor Z where
-   fmap f (Z ls c rs) = Z (map f ls) (f c) (map f rs)
-
 instance Comonad Stream where
    extract (Cons a _) = a
    duplicate = iterate tail
@@ -42,8 +81,14 @@ instance Comonad Z where
    extract (Z _ c _) = c
    duplicate = zipIterate zipL zipR
 
+instance Applicative Z where
+   pure = Z <$> pure <*> id <*> pure
+   (Z ls c rs) <*> (Z ls' c' rs') =
+      Z (ls <*> ls') (c c') (rs <*> rs')
+
 instance Distributive Stream where
-   distribute = unfold (fmap extract &&& fmap tail)
+   distribute =
+      unfold (fmap extract &&& fmap tail)
 
 instance Distributive Z where
    distribute =
@@ -51,11 +96,13 @@ instance Distributive Z where
                 (fmap extract)
                 (fmap extract &&& fmap zipR)
 
-composedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
-composedly f = Compose . f . getCompose
+instance ComonadApply Stream where (<@>) = (<*>)
+instance ComonadApply Z      where (<@>) = (<*>)
 
-composedly2 f = Compose . Compose . f . getCompose . getCompose
-
-instance (Comonad f, Comonad g, Distributive f, Distributive g) => Comonad (Compose f g) where
+instance (Comonad f, Comonad g, Distributive g) => Comonad (Compose f g) where
    extract   = extract . extract . getCompose
-   duplicate = Compose . (fmap . fmap) Compose . collect duplicate . collect duplicate . getCompose
+   duplicate = fmap Compose . Compose -- wrap it again: f (g (f (g a))) -> Compose f g (Compose f g a)
+             . fmap distribute        -- swap middle two layers: f (f (g (g a))) -> f (g (f (g a)))
+             . duplicate              -- duplicate outer functor f: f (g (g a)) -> f (f (g (g a)))
+             . fmap duplicate         -- duplicate inner functor g: f (g a) -> f (g (g a))
+             . getCompose             -- unwrap it: Compose f g a -> f (g a)
