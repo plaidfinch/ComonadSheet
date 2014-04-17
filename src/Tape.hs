a3d71bcdf42732ba7a6fde53a90232990373d828
Kenneth Foner <kenny.foner@gmail.com>
2014-04-17 16:03:59 -0400

    Added a crapton of documentation comments to Tape.

--- src/Tape.hs
+++ src/Tape.hs
@@ -16,30 +16,55 @@ import Prelude hiding ( iterate )
 -- | A Tape is like a Turing-machine tape: infinite in both directions, with a focus in the middle.
 data Tape a = Tape (Stream a) a (Stream a) deriving ( Functor )
 
-unfold :: (c -> (a,c)) -> (c -> a) -> (c -> (a,c)) -> c -> Tape a
+-- | Produce a Tape from a seed value, ala unfoldr for lists, or unfold for 'Stream's.
+unfold :: (c -> (a,c)) -- ^ leftwards unfolding function
+       -> (c -> a)     -- ^ function giving the focus value from the seed
+       -> (c -> (a,c)) -- ^ rightwards unfolding function
+       -> c            -- ^ seed value
+       -> Tape a
 unfold prev center next =
    Tape <$> S.unfold prev <*> center <*> S.unfold next
 
-iterate :: (a -> a) -> (a -> a) -> a -> Tape a
+-- | Produce a Tape consisting of the infinite iteration of two functions to a starting focus value,
+--   ala iterate for lists or Streams.
+iterate :: (a -> a) -- ^ leftwards iteration function
+        -> (a -> a) -- ^ rightwards iteration function
+        -> a        -- ^ focus value
+        -> Tape a
 iterate prev next =
    unfold (dup . prev) id (dup . next)
    where dup a = (a,a)
 
+-- | Given an enumerable type, produce the Tape where the left side is the sequence of predecessors,
+--   and the right side is the sequence of successors.
 enumerate :: (Enum a) => a -> Tape a
 enumerate = iterate pred succ
 
+-- | Tapes form a comonad, where extract gives the focus element and duplicate gives a "diagonalized"
+--   @Tape (Tape a)@ such that @extract . extract . zipL . duplicate == extract . zipL@ and likewise
+--   for @zipR@. 
 instance Comonad Tape where
    extract (Tape _ c _) = c
    duplicate = iterate zipL zipR
 
+-- | "Applying" one tape to another zips them together. This is like the Applicative instances for
+--   'ZipList' and 'Stream'.
 instance ComonadApply Tape where
    (Tape ls c rs) <@> (Tape ls' c' rs') =
       Tape (ls <@> ls') (c c') (rs <@> rs')
 
+-- | A tape is 'Applicative', where the "apply" is the same as its ComonadApply instance (required by
+--   law), and a pure value is the tape consisting of copies of that value in both directions.
 instance Applicative Tape where
    (<*>) = (<@>)
    pure  = Tape <$> pure <*> id <*> pure
 
+-- | Tapes are 'Distributive' because we can replicate their structure on the outside of a functor by
+--   sending movement commands through the functor via @fmap zipL@ and @fmap zipR@, and using
+--   @fmap extract@ to remove the extra structure inside the functor. As stated in the Distributive
+--   documentation, this can only work if all Tapes have the same cardinality of holes, and if there
+--   is no extra information to propagate from outside the functor -- hence, an 'Indexed' tape can't
+--   be made into a Distributive, as there's no way to extract the index from the functor.
 instance Distributive Tape where
    distribute =
       unfold (fmap extract &&& fmap zipL)
@@ -48,46 +73,72 @@ instance Distributive Tape where
 
 -- Now let's get multi-dimensional!
 
+-- | A @Tape2@ is a 2-dimensional tape formed by the composition of two 1-dimensional tapes.
 type Tape2 = Compose Tape  Tape
+-- | A @Tape3@ is a 3-dimensional tape formed by the composition of a 2D tape and a 1D tape.
 type Tape3 = Compose Tape2 Tape
+-- | A @Tape4@ is a 4-dimensional tape formed by the composition of a 3D tape and a 1D tape.
 type Tape4 = Compose Tape3 Tape
 
+-- | Any tape of one dimension or higher can be zipped left or right.
 class Dimension1 z where
-  zipL :: z a -> z a
-  zipR :: z a -> z a
+  -- | Moves left by one step (if it's indexed, this is the negative direction).
+  zipL :: z a -> z a 
+  -- | Moves right by one step (if it's indexed, this is the positive direction).
+  zipR :: z a -> z a 
 
+-- | Any tape of two dimensions or higher can be zipped up or down.
 class Dimension2 z where
-  zipU :: z a -> z a
-  zipD :: z a -> z a
+  -- | Moves up by one step (if indexed, negative direction).
+  zipU :: z a -> z a 
+  -- | Moves down by one step (if indexed, positive direction).
+  zipD :: z a -> z a 
 
+-- | Any tape of three dimensions or higher can be zipped in or out.
 class Dimension3 z where
-  zipI :: z a -> z a
-  zipO :: z a -> z a
+  -- | Moves in by one step (if indexed, negative direction).
+  zipI :: z a -> z a 
+  -- | Moves out by one step (if indexed, positive direction).
+  zipO :: z a -> z a 
 
+-- | Any tape of four dimensions or higher can be zipped /ana/ or /kata/ (following the nomenclature
+--   of <http://en.wikipedia.org/wiki/Charles_Howard_Hinton Charles Howard Hinton>).
 class Dimension4 z where
-  zipA :: z a -> z a
-  zipK :: z a -> z a
+  -- | Moves /ana/ by one step (if indexed, negative direction).
+  zipA :: z a -> z a 
+  -- | Moves /kata/ by one step (if indexed, positive direction).
+  zipK :: z a -> z a 
 
 -- | Apply a function to the inside of a Compose.
+--
+--   @composedly f = Compose . f . getCompose@
 composedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
 composedly f = Compose . f . getCompose
 
+-- | A single tape is one-dimensional in the obvious way. This is the base-case instance that allows
+--   all the higher-dimensional instances to work properly.
 instance Dimension1 Tape where
   zipR (Tape ls c (Cons r rs)) = Tape (Cons c ls) r rs
   zipL (Tape (Cons l ls) c rs) = Tape ls l (Cons c rs)
 
+-- | Telling a nested tape to move in the first axis always moves the inner-most tape, regardless of
+--   how deeply nested it is.
 instance (Functor f) => Dimension1 (Compose f Tape) where
   zipL = composedly (fmap zipL)
   zipR = composedly (fmap zipR)
 
+-- | Telling a nested tape to move in the second axis always moves the second-inner-most tape,
+--   regardless of the depth of nesting.
 instance (Dimension1 z) => Dimension2 (Compose z Tape) where
   zipU = composedly zipL
   zipD = composedly zipR
 
+-- | Telling a nested tape to move in the third axis always moves the third-inner-most tape.
 instance (Dimension2 z) => Dimension3 (Compose z Tape) where
   zipI = composedly zipU
   zipO = composedly zipD
 
+-- | Telling a nested tape to move in the fourth axis always moves the fourth-inner-most tape.
 instance (Dimension3 z) => Dimension4 (Compose z Tape) where
   zipA = composedly zipI
   zipK = composedly zipO
@@ -127,6 +178,6 @@ cross4 a b c d = (,,,) <$> (Compose . Compose . Compose) (     pure .      pure 
                        <*> (Compose . Compose . Compose) (     pure . fmap pure . fmap pure $ c)
                        <*> (Compose . Compose . Compose) (fmap pure . fmap pure . fmap pure $ d)
 
--- Some example zippers for testing...
-ints :: Tape Int
+-- | The tape of integers, with zero centered.
+ints :: Tape Integer
 ints = enumerate 0
