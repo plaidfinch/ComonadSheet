9b019533323f1965e8da8cea2c48ec80506e315d
Kenneth Foner <kenny.foner@gmail.com>
2014-04-18 17:54:09 -0400

    Added insert1, insert2, ...

--- src/Slice.hs
+++ src/Slice.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances      #-}
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE UndecidableInstances   #-}
 
 module Slice where
@@ -49,51 +50,64 @@ data Signed f a = Positive (f a)
                 | Negative (f a)
                 deriving ( Eq , Ord , Show )
 
-class InsertCompose l t where
-   insertCompose :: l a -> t a -> t a
+class InsertC l t where
+   insertC :: l a -> t a -> t a
 
 -- | Given the @Compose@ of two list-like things and the @Compose@ of two @Tape@-like things, we can
 --   insert the list-like things into the @Tape@-like things if we know how to insert each corresponding
 --   level with one another. Thus, other than this instance, all the other instances we need to define
 --   are base cases: how to insert a single list-like thing into a single @Tape@.
-instance (Functor l, Applicative f, InsertCompose l f, InsertCompose m g) => InsertCompose (Compose l m) (Compose f g) where
-   insertCompose (Compose lm) (Compose fg) =
-      Compose $ insertCompose (fmap insertCompose lm) (pure id) <*> fg
+instance (Functor l, Applicative f, InsertC l f, InsertC m g) => InsertC (Compose l m) (Compose f g)
+   where
+      insertC (Compose lm) (Compose fg) =
+         Compose $ insertC (fmap insertC lm) (pure id) <*> fg
 
-instance InsertCompose Tape Tape where
-   insertCompose t _ = t
+instance InsertC Tape Tape where
+   insertC t _ = t
 
-instance InsertCompose Stream Tape where
-   insertCompose (Cons x xs) (Tape ls _ _) = Tape ls x xs
+instance InsertC Stream Tape where
+   insertC (Cons x xs) (Tape ls _ _) = Tape ls x xs
 
-instance InsertCompose (Signed Stream) Tape where
-   insertCompose (Positive (Cons x xs)) (Tape ls _ _) = Tape ls x xs
-   insertCompose (Negative (Cons x xs)) (Tape _ _ rs) = Tape xs x rs
+instance InsertC (Signed Stream) Tape where
+   insertC (Positive (Cons x xs)) (Tape ls _ _) = Tape ls x xs
+   insertC (Negative (Cons x xs)) (Tape _ _ rs) = Tape xs x rs
 
-instance InsertCompose [] Tape where
-   insertCompose [] t = t
-   insertCompose (x : xs) (Tape ls c rs) =
+instance InsertC [] Tape where
+   insertC [] t = t
+   insertC (x : xs) (Tape ls c rs) =
       Tape ls x (S.prefix xs (Cons c rs))
 
-instance InsertCompose (Signed []) Tape where
-   insertCompose (Positive []) t = t
-   insertCompose (Negative []) t = t
-   insertCompose (Positive (x : xs)) (Tape ls c rs) =
+instance InsertC (Signed []) Tape where
+   insertC (Positive []) t = t
+   insertC (Negative []) t = t
+   insertC (Positive (x : xs)) (Tape ls c rs) =
       Tape ls x (S.prefix xs (Cons c rs))
-   insertCompose (Negative (x : xs)) (Tape ls c rs) =
+   insertC (Negative (x : xs)) (Tape ls c rs) =
       Tape (S.prefix xs (Cons c ls)) x rs
+
+insert1 :: (InsertC f Tape) => f a -> Tape a -> Tape a
+insert1 = insertC
+
+insert2 :: (InsertC (Compose f g) Tape2) => f (g a) -> Tape2 a -> Tape2 a
+insert2 = insertC . Compose
+
+insert3 :: (InsertC (Compose (Compose f g) h) Tape3) => f (g (h a)) -> Tape3 a -> Tape3 a
+insert3 = insertC . Compose . Compose
+
+insert4 :: (InsertC (Compose (Compose (Compose f g) h) i) Tape4) => f (g (h (i a))) -> Tape4 a -> Tape4 a
+insert4 = insertC . Compose . Compose . Compose
 
 class Insert l t where
    insert :: l -> t -> t
 
-instance (InsertCompose f Tape) => Insert (f a) (Tape a) where
-   insert = insertCompose
+instance (InsertC f Tape) => Insert (f a) (Tape a)
+   where insert = insert1
 
-instance (InsertCompose (Compose f g) Tape2) => Insert (f (g a)) (Tape2 a) where
-   insert = insertCompose . Compose
+instance (InsertC (Compose f g) Tape2) => Insert (f (g a)) (Tape2 a)
+   where insert = insert2
 
-instance (InsertCompose (Compose (Compose f g) h) Tape3) => Insert (f (g (h a))) (Tape3 a) where
-   insert = insertCompose . Compose . Compose
+instance (InsertC (Compose (Compose f g) h) Tape3) => Insert (f (g (h a))) (Tape3 a)
+   where insert = insert3
 
-instance (InsertCompose (Compose (Compose (Compose f g) h) i) Tape4) => Insert (f (g (h (i a)))) (Tape4 a) where
-   insert = insertCompose . Compose . Compose . Compose
+instance (InsertC (Compose (Compose (Compose f g) h) i) Tape4) => Insert (f (g (h (i a)))) (Tape4 a)
+   where insert = insert4
