433e50ecd7b86a07e6423b11c45f3189c309e775
Kenneth Foner <kenny.foner@gmail.com>
2014-04-18 19:05:03 -0400

    Transitioned MPTC/FD in Slice to use type families -- now there are no functional dependencies in the whole new version of the project.

--- src/Slice.hs
+++ src/Slice.hs
@@ -1,50 +1,66 @@
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE UndecidableInstances   #-}
 
 module Slice where
 
 import Control.Applicative
 import Data.Functor.Compose
+
 import Stream ( Stream(..) )
 import qualified Stream as S
+
 import Prelude hiding ( iterate , take )
 
 import Tape
 
-class Take i t l | t -> i l where
-   take :: i -> t -> l
+class Take t where
+   type Index t
+   type List  t
+   take :: Index t -> t -> List t
 
-instance Take Int (Tape a) [a] where
+instance Take (Tape a) where
+   type Index (Tape a) = Int
+   type List  (Tape a) = [a]
    take i t | i > 0 = focus t : S.take     (i - 1) (viewR t)
    take i t | i < 0 = focus t : S.take (abs i - 1) (viewL t)
    take _ _ = []
 
-instance Take (Int,Int) (Tape2 a) [[a]] where
+instance Take (Tape2 a) where
+   type Index (Tape2 a) = (Int,Int)
+   type List  (Tape2 a) = [[a]]
    take (i,j) = take j . fmap (take i) . getCompose
 
-instance Take (Int,Int,Int) (Tape3 a) [[[a]]] where
+instance Take (Tape3 a) where
+   type Index (Tape3 a) = (Int,Int,Int)
+   type List  (Tape3 a) = [[[a]]]
    take (i,j,k) = take (j,k) . fmap (take i) . getCompose
 
-instance Take (Int,Int,Int,Int) (Tape4 a) [[[[a]]]] where
+instance Take (Tape4 a) where
+   type Index (Tape4 a) = (Int,Int,Int,Int)
+   type List  (Tape4 a) = [[[[a]]]]
    take (i,j,k,l) = take (j,k,l) . fmap (take i) . getCompose
 
-class Window i t l | t -> i l where
-   window :: i -> i -> t -> l
+class Window t where
+   window :: Index t -> Index t -> t -> List t
 
-instance Window Int (Tape a) [a] where
-   window i i' t = reverse (take (negate (abs i)) t) ++ tail (take (abs i') t)
+instance Window (Tape a) where
+   window i i' t =
+      reverse (take (negate (abs i)) t) ++ tail (take (abs i') t)
 
-instance Window (Int,Int) (Tape2 a) [[a]] where
-   window (i,j) (i',j') = window i i' . fmap (window j j') . getCompose
+instance Window (Tape2 a) where
+   window (i,j) (i',j') =
+      window i i' . fmap (window j j') . getCompose
 
-instance Window (Int,Int,Int) (Tape3 a) [[[a]]] where
-   window (i,j,k) (i',j',k') = window (i,j) (i',j') . fmap (window k k') . getCompose
+instance Window (Tape3 a) where
+   window (i,j,k) (i',j',k') =
+      window (i,j) (i',j') . fmap (window k k') . getCompose
 
-instance Window (Int,Int,Int,Int) (Tape4 a) [[[[a]]]] where
-   window (i,j,k,l) (i',j',k',l') = window (i,j,k) (i',j',k') . fmap (window l l') . getCompose
+instance Window (Tape4 a) where
+   window (i,j,k,l) (i',j',k',l') =
+      window (i,j,k) (i',j',k') . fmap (window l l') . getCompose
 
 data Signed f a = Positive (f a)
                 | Negative (f a)
--- src/Tape.hs
+++ src/Tape.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE FlexibleInstances      #-}
-{-# LANGUAGE FlexibleContexts       #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE DeriveFunctor          #-}
 
 module Tape where
