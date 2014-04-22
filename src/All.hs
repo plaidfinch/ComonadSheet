aa23ff9ee58de19e8b1ba12d5deef39e3c9dad0f
Kenneth Foner <kenny.foner@gmail.com>
2014-04-22 13:21:03 -0400

    Added 'All' module to facilitate debugging how things fit together. Simply re-exports everything.

---
+++ src/All.hs
@@ -0,0 +1,20 @@
+module All
+   ( module Names
+   , module Generic
+   , module Reference
+   , module Indexed
+   , module Tape
+   , module Composition
+   , module Peano
+   , module Control.Comonad
+   ) where
+
+import Names
+import Generic
+import Reference
+import Indexed
+import Tape
+import Composition
+import Peano
+
+import Control.Comonad
