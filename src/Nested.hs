c3a8046a4340e0b52ef963fd68649cbcd9eb57a8
Kenneth Foner <kenny.foner@gmail.com>
2014-06-01 12:22:58 -0700

    Increased laziness in Nested instances; moved notes from comment in Nested to separate file in Notes/.

--- src/Nested.hs
+++ src/Nested.hs
@@ -36,31 +36,29 @@ unNest (Flat x) = x
 unNest (Nest x) = x
 
 instance (Functor f) => Functor (Nested (Flat f)) where
-   fmap f (Flat x) = Flat (fmap f x)
+   fmap f = Flat . fmap f . unNest
 
 instance (Functor f, Functor (Nested fs)) => Functor (Nested (Nest fs f)) where
-   fmap f (Nest x) = Nest (fmap (fmap f) x)
+   fmap f = Nest . fmap (fmap f) . unNest
 
 instance (Applicative f) => Applicative (Nested (Flat f)) where
-   pure = Flat . pure
-   Flat f <*> Flat x = Flat (f <*> x)
+   pure    = Flat . pure
+   f <*> x = Flat (unNest f <*> unNest x)
 
 instance (Applicative f, Applicative (Nested fs)) => Applicative (Nested (Nest fs f)) where
-   pure = Nest . pure . pure
-   Nest f <*> Nest x = Nest ((<*>) <$> f <*> x)
+   pure    = Nest . pure . pure
+   f <*> x = Nest ((<*>) <$> unNest f <*> unNest x)
+
+instance (ComonadApply f) => ComonadApply (Nested (Flat f)) where
+   f <@> x = Flat (unNest f <@> unNest x)
+
+instance (ComonadApply f, Distributive f, ComonadApply (Nested fs)) => ComonadApply (Nested (Nest fs f)) where
+   f <@> x = Nest ((<@>) <$> unNest f <@> unNest x)
 
 instance (Comonad f) => Comonad (Nested (Flat f)) where
    extract   = extract . unNest
    duplicate = fmap Flat . Flat . duplicate . unNest
 
---   This might be an overly-general instance, because it would conflict with another possible
---   (valid) definition for the composition of two comonads, but which requires constraints we can't
---   satisfy with the comonads we're using. Namely, you can also instantiate this instance if you
---   replace @fmap distribute@ with @fmap sequenceA@ (from @Data.Traversable@), and let the resulting
---   instance have the constraints @(Comonad f, Comonad g, Traversable f, Applicative g)@. Since not
---   all of our comonads in this project are applicative, and no infinite structure (e.g. @Tape@) has
---   a good (i.e. not returning bottom in unpredictable ways) definition of @Traversable@, using the
---   @Distributive@ constraint makes much more sense here.
 instance (Comonad f, Comonad (Nested fs), Distributive f, Functor (Nested (Nest fs f))) => Comonad (Nested (Nest fs f)) where
    extract   = extract . extract . unNest
    duplicate =
@@ -69,13 +67,6 @@ instance (Comonad f, Comonad (Nested fs), Distributive f, Functor (Nested (Nest 
       . duplicate        -- duplicate outer functor f: f (g (g a)) -> f (f (g (g a)))
       . fmap duplicate   -- duplicate inner functor g: f (g a) -> f (g (g a))
       . unNest           -- NOTE: can't pattern-match on constructor or you break laziness!
-
-instance (ComonadApply f) => ComonadApply (Nested (Flat f)) where
-   Flat f <@> Flat x = Flat (f <@> x)
-
-instance (ComonadApply f, Distributive f, ComonadApply (Nested fs)) => ComonadApply (Nested (Nest fs f)) where
-   Nest f <@> Nest x =
-      Nest ((<@>) <$> f <@> x)
 
 type family AddNest x where
    AddNest (Nested fs (f x)) = Nested (Nest fs f) x
