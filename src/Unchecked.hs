b821aa2dbedba4d06a02271dd0fca1e0a44056b1
Kenneth Foner <kenny.foner@gmail.com>
2014-06-01 15:22:22 -0700

    Made type signatures crisper in Unchecked.

--- src/Unchecked.hs
+++ src/Unchecked.hs
@@ -24,12 +24,12 @@ cell = (extract .) . go
 cells :: (Traversable t, Comonad w, Go r w) => t (RefList r) -> w a -> t a
 cells = traverse cell
 
-sheet :: ( InsertNested l t , Applicative t
-         , DimensionalAs x (t a) , AsDimensionalAs x (t a) ~ l a )
-         => a -> x -> t a
+sheet :: ( InsertNested l (Nested ts) , Applicative (Nested ts)
+         , DimensionalAs x (Nested ts a) , AsDimensionalAs x (Nested ts a) ~ l a )
+         => a -> x -> Nested ts a
 sheet background functions = insert functions (pure background)
 
 indexedSheet :: ( InsertNested l (Nested ts) , Applicative (Nested ts)
-                , NestedAs x (Nested ts a) , AsNestedAs x (Nested ts a) ~ l a)
+                , DimensionalAs x (Nested ts a) , AsDimensionalAs x (Nested ts a) ~ l a)
                 => Coordinate (NestedCount ts) -> a -> x -> Indexed ts a
 indexedSheet i = (Indexed i .) . sheet
